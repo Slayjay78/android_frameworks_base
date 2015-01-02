c3b298b71d624b5655fc884cd13feb8d9db98ba5
dankoman <dankoman30@gmail.com>
2015-01-02 16:13:34 +0100

    Fw_base - Proximity speaker [1/3]
    Patchset: add time delay preference
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    Change-Id: Iad137354b668f1fc8b0044ea7f615b86c315edf3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3672,6 +3672,25 @@ public final class Settings {
         public static final String SWAP_VOLUME_BUTTONS = "swap_volume_buttons";
 
         /**
+         * Whether the proximity sensor will adjust call to speaker
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER = "proximity_auto_speaker";
+
+        /**
+         * Time delay to activate speaker after proximity sensor triggered
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_DELAY = "proximity_auto_speaker_delay";
+
+        /**
+         * Whether the proximity sensor will adjust call to speaker,
+         * only while in call (not while ringing on outgoing call)
+         * @hide
+         */
+        public static final String PROXIMITY_AUTO_SPEAKER_INCALL_ONLY = "proximity_auto_speaker_incall_only";
+
+        /**
          * Show search bar in recents
          * @hide
          */
