68fb47245e6cfcca2f354e5d1eece7b7e443da9b
Adnan <adnan@cyngn.com>
2015-05-03 22:56:10 +0900

    SystemUI: HeadsUp blacklists (1/2)
    BenzoEdit:
    Made to work without cmsdk
    
    Change-Id: I5bb9ead502d71182a689a3cb63f7ba9f8414d5b6
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3918,6 +3918,20 @@ public final class Settings {
         public static final String STATUSBAR_BATTERY_BAR_ANIMATE = "statusbar_battery_bar_animate";
 
         /**
+         * Which applications to disable heads up notifications in
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_CUSTOM_VALUES = "heads_up_custom_values";
+
+        /**
+         * Which applications to disable heads up notifications for
+         *
+         * @hide
+         */
+        public static final String HEADS_UP_BLACKLIST_VALUES = "heads_up_blacklist_values";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
