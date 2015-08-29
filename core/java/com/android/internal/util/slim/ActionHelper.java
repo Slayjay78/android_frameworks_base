62ad1ea992023102c9f90145212da11bee33c964
SpiritCroc <spiritcroc@gmail.com>
2015-08-29 11:43:21 +0200

    [1/2] Slim Recent App Sidebar
    One thing I was missing with SlimRecents was the possibility to launch your favorite apps from the recents screen. As SlimRecents covers only the half of the screen (more or less), there is enough space on the other edge to display an app sidebar, which I based on the ChameleonOS one.
    
    Changes to the ChameleonOS sidebar include:
    - No trigger areas, but show/hide with the SlimRecent screen
    - Adapted look and animation to SlimRecent
    - Longpress actions
    - Slimer settings using ActionListViewSettings, also added possibility to configure sidebar size (roughly, as the size is adapted to the screen height in order to show full icons) and colors
    - Currently no option to add folders to the sidebar, as this does not work with the current ActionListViewSettings without further work
    
    Benzo Edit:
    Squashed: Slim Recent App Sidebar make format look nicer
    
    Change-Id: I1f3edce2ed12297a7a7cb976f2a2315f86492192
    
    Conflicts:
    	core/java/com/android/internal/util/slim/ActionHelper.java

--- core/java/com/android/internal/util/slim/ActionHelper.java
+++ core/java/com/android/internal/util/slim/ActionHelper.java
@@ -16,6 +16,7 @@
 
 package com.android.internal.util.slim;
 
+import android.content.ContentResolver; 
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -24,6 +25,8 @@ import android.content.res.Resources;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
+import android.provider.Settings;
+import android.os.UserHandle;
 import android.util.Log;
 
 import java.io.File;
@@ -34,6 +37,40 @@ import java.util.ArrayList;
 public class ActionHelper {
 
     private static final String SYSTEMUI_METADATA_NAME = "com.android.systemui";
+
+    public static ArrayList<ActionConfig> getRecentAppSidebarConfig(Context context) {
+        return (ConfigSplitHelper.getActionConfigValues(context,
+                getRecentAppSidebarProvider(context), null, null, false));
+    }
+
+    public static ArrayList<ActionConfig> getRecentAppSidebarConfigWithDescription(
+            Context context, String values, String entries) {
+        return (ConfigSplitHelper.getActionConfigValues(context,
+                getRecentAppSidebarProvider(context), values, entries, false));
+    }
+
+    private static String getRecentAppSidebarProvider(Context context) {
+        String config = Settings.System.getStringForUser(
+                context.getContentResolver(),
+                Settings.System.RECENT_APP_SIDEBAR_CONTENT,
+                UserHandle.USER_CURRENT);
+        if (config == null) {
+            config = "";
+        }
+        return config;
+    }
+
+    public static void setRecentAppSidebarConfig(
+            Context context, ArrayList<ActionConfig> actionConfig, boolean reset) {
+        String config;
+        if (reset) {
+            config = "";
+        } else {
+            config = ConfigSplitHelper.setActionConfig(actionConfig, false);
+        }
+        Settings.System.putString(context.getContentResolver(),
+                Settings.System.RECENT_APP_SIDEBAR_CONTENT, config);
+    }
 
     // General methods to retrieve the correct icon for the respective action.
     public static Drawable getButtonIconImage(Context context,
