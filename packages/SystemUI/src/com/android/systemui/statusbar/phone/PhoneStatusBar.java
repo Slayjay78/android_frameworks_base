e7fe7445f861a493229fb254f70471ca6d31c314
Roman Birg <roman@cyngn.com>
2015-10-26 11:44:11 -0700

    Fix some keyguard wallpaper issues
    Partial cherry pick of Change-Id:
    I0bf7d5ffdf8972cf10ba1a90ae9c5d3f9a6a9aab
    
    - Cache wallpaper in PhoneStatusBar so we don't query it every time
      media meta data tries to update
    
    - Don't crash if service isn't connected yet, return null
    
    Change-Id: I8f0c8a53b634db5ffd27be3975b7b2db1b35bbfb
    Signed-off-by: Roman Birg <roman@cyngn.com>
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -344,6 +344,8 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     private long mKeyguardFadingAwayDelay;
     private long mKeyguardFadingAwayDuration;
 
+    private Bitmap mKeyguardWallpaper;
+
     int mKeyguardMaxNotificationCount;
 
     boolean mExpandedVisible;
@@ -731,6 +733,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.Global.getUriFor(SETTING_HEADS_UP_TICKER), true,
                     mHeadsUpObserver);
         }
+
+        WallpaperManager wallpaperManager = (WallpaperManager) mContext.getSystemService(
+                Context.WALLPAPER_SERVICE);
+        mKeyguardWallpaper = wallpaperManager.getKeyguardBitmap();
+
         mUnlockMethodCache = UnlockMethodCache.getInstance(mContext);
         mUnlockMethodCache.addListener(this);
         startKeyguard();
@@ -1816,11 +1823,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
 
         // apply user lockscreen image
         if (backdropBitmap == null) {
-            WallpaperManager wm = (WallpaperManager)
-                    mContext.getSystemService(Context.WALLPAPER_SERVICE);
-            if (wm != null) {
-                backdropBitmap = wm.getKeyguardBitmap();
-            }
+            backdropBitmap = mKeyguardWallpaper;
         }
 
         final boolean hasBackdrop = backdropBitmap != null;
@@ -3160,6 +3163,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     updateMediaMetaData(true);
                 }
             } else if (Intent.ACTION_KEYGUARD_WALLPAPER_CHANGED.equals(action)) {
+                WallpaperManager wm = (WallpaperManager) mContext.getSystemService(
+                        Context.WALLPAPER_SERVICE);
+                mKeyguardWallpaper = wm.getKeyguardBitmap();
                 updateMediaMetaData(true);
             }
         }
@@ -3213,6 +3219,11 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     public void userSwitched(int newUserId) {
         super.userSwitched(newUserId);
         if (MULTIUSER_DEBUG) mNotificationPanelDebugText.setText("USER " + newUserId);
+        WallpaperManager wm = (WallpaperManager)
+                mContext.getSystemService(Context.WALLPAPER_SERVICE);
+        mKeyguardWallpaper = null;
+        wm.forgetLoadedKeyguardWallpaper();
+
         animateCollapsePanels();
         updatePublicMode();
         updateNotifications();
@@ -3220,6 +3231,7 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
         setControllerUsers();
         mAssistManager.onUserSwitched(newUserId);
 
+        mKeyguardWallpaper = wm.getKeyguardBitmap();
         if (mNavigationBarView != null) {
             mNavigationBarView.updateSettings();
         }
